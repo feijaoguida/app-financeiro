b4697d24726cb4982c1dcbcf6da6fa6d09a4f1b8
Roseweltty <roseweltty@visie.com.br>
2021-12-11 15:08:52 -0300

    feat: arquivos para armazenar informações

---
+++ src/data/items.ts
@@ -0,0 +1,26 @@
+export const items = [
+  {
+    date: new Date(2021, 9, 15),
+    category: 'food',
+    title: 'McDonalds',
+    value: 32.12
+  },
+  {
+    date: new Date(2021, 9, 15),
+    category: 'food',
+    title: 'Burger King',
+    value: 28
+  },
+  {
+    date: new Date(2021, 9, 16),
+    category: 'rent',
+    title: 'Aluguel Apt',
+    value: 2300
+  },
+  {
+    date: new Date(2021, 10, 18),
+    category: 'salary',
+    title: 'Salário xpto',
+    value: 4500
+  }
+]
