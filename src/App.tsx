a621705c1ea550acffa021afe52362cd5288366e
Roseweltty <roseweltty@visie.com.br>
2021-12-11 15:08:06 -0300

    feat: Criação de styles componentes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import * as C from './App.styles';
 const App = () => {
   return (
     <C.Container>
-      ...
+      <C.Header>
+        <C.HeaderText>Sistema Financeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        {/* Área de Informações */}
+        {/* Área de inserção */}
+        {/* Tabela de itens*/}
+      </C.Body>
     </C.Container>
   );
 }
